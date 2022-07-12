855ea0a5b0a82b089489272d72ff2a5ecd48aba8
sabrinaValansi <sabrivalan@hotmail.com>
2022-07-12 20:16:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <Vista/>
+    <h2><i>RESPUESTAS</i></h2>
+        <div class="table responsive">
+          <table class="table table-dark">
+            <tr>
+              <th>Pregunta</th>
+              <th>Respuesta</th>
+            </tr>
+            <tr>
+             <th>1</th>
+             <th>B</th>
+            </tr>
+            <tr>
+             <th>2</th>
+             <th>C</th>
+            </tr>
+            <tr>
+             <th>3</th>
+             <th>A</th>
+            </tr>
+            <tr>
+             <th>4</th>
+             <th>C,D</th>
+            </tr>
+            <tr>
+             <th>5</th>
+             <th>A</th>
+            </tr>
+            
+
+            
+            
+          </table>
+      </div>
+  </div>
+</template>
+
+<script>
+import Vista from './components/Vista.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Vista
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Vista.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <section class="src-components-vista">
+
+   <label for="color">Color:</label>
+   <input type="text" name="color" id="color" v-model="color">
+
+   <label for="fondo">Fondo:</label>
+   <input type="text" name="fondo" id="fondo" v-model="fondo">
+
+   <label for="size">Size:</label>
+   <input type="text" name="size" id="size" v-model="size">
+
+   <br><br><hr>
+
+   <label for="text">Texto:</label>
+   <input type="text" name="texto" id="texto"  :style="{ background:fondo, color: color, fontSize:size }" v-model.trim="texto">
+
+  <p v-if="texto!=''">Cantidad de palabras : {{contarPalabras}}</p>
+  
+
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-vista',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        color:'red',
+        fondo:'pink',
+        size:'15px',
+        texto:'',
+        cant:0
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      contarPalabras(){
+        let cont=0
+        const arrayPalabras = this.texto.split(' ')
+        console.log(arrayPalabras);
+        for (let i = 0; i < arrayPalabras.length; i++) {
+          cont++;
+        }
+        return cont
+      }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-vista {
+    
+  }
+  hr {
+    background-color: #bbb;
+  }
+</style>
